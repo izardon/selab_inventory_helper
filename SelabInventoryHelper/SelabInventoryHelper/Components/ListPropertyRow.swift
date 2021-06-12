475bcaed4ad385a6f280e4d21d0db6643f0baa3b
chan <izardon@gmail.com>
2021-06-12 17:36:52 +0800

    fixed the bug of showing alter on list property view icon

--- SelabInventoryHelper/SelabInventoryHelper/Components/ListPropertyRow.swift
+++ SelabInventoryHelper/SelabInventoryHelper/Components/ListPropertyRow.swift
@@ -11,98 +11,97 @@ import SwiftUI
 struct ListPropertyRow: View {
     @State var property: Property
     @State var isScrapped: Bool
-    @State private var showingAlert = false
+    @State private var showingScrapAlert = false
+    @State private var showingTrashAlert = false
     
     var body: some View {
-        HStack {
-            NavigationLink(destination: PropertyDetail(property: property)) {
-                HStack {
-                    Image("testImg")
-                        .resizable()
-                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    
-                    VStack(alignment: .leading) {
-                        Text(property.identify)
-                            .font(.title2)
-                        Text(property.name)
-                            .font(.body)
-                        Text(property.location)
-                            .font(.body)
+        NavigationLink(destination: PropertyDetail(property: property)) {
+            HStack {
+                Image("testImg")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                VStack(alignment: .leading) {
+                    Text(property.identify)
+                        .font(.title2)
+                    Text(property.name)
+                        .font(.body)
+                    Text(property.location)
+                        .font(.body)
+                }
+                
+                Spacer()
+                
+                if isScrapped {
+                    VStack {
+                        Spacer()
+                        Image(systemName: "tray.and.arrow.up")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                print("tray.and.arrow.up")
+                                showingScrapAlert = true
+                            }
+                            .alert(isPresented: $showingScrapAlert) {
+                                Alert(
+                                    title: Text("取消報廢這筆財產？"),
+                                    message: Text("可再報廢"),
+                                    primaryButton: .destructive(Text("確定")) {
+                                        print("tray.and.arrow.up...")
+                                    },
+                                    secondaryButton: .cancel(Text("取消"))
+                                )
+                            }
+                        
+                        Spacer()
                     }
-                    
-                    Spacer()
-                    
-                    if isScrapped {
-                        VStack {
-                            Spacer()
-                            Image(systemName: "tray.and.arrow.up")
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(.blue)
-                                .onTapGesture {
-                                    print("tray.and.arrow.up")
-                                    showingAlert = true
-                                }
-                                .alert(isPresented: $showingAlert) {
-                                    Alert(
-                                        title: Text("取消報廢這筆財產？"),
-                                        message: Text("可再報廢"),
-                                        primaryButton: .destructive(Text("確定")) {
-                                            print("tray.and.arrow.up...")
-                                        },
-                                        secondaryButton: .cancel(Text("取消"))
-                                    )
-                                }
-
-                            Spacer()
-                        }
-                    } else {
-                        VStack {
-                            Spacer()
-                            Image(systemName: "tray.and.arrow.down")
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(.blue)
-                                .onTapGesture {
-                                    print("tray.and.arrow.down")
-                                    showingAlert = true
-                                }
-                                .alert(isPresented: $showingAlert) {
-                                    Alert(
-                                        title: Text("報廢這筆財產？"),
-                                        message: Text("可再還原"),
-                                        primaryButton: .destructive(Text("報廢")) {
-                                            print("Archivebox...")
-                                        },
-                                        secondaryButton: .cancel(Text("取消"))
-                                    )
-                                }
-                            
-                            Spacer()
-                            Image(systemName: "trash")
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(.red)
-                                .onTapGesture {
-                                    print("trash")
-                                    showingAlert = true
-                                }
-                                .alert(isPresented: $showingAlert) {
-                                    Alert(
-                                        title: Text("確定要刪除這筆財產？"),
-                                        message: Text("無法還原"),
-                                        primaryButton: .destructive(Text("刪除")) {
-                                            print("Deleting...")
-                                        },
-                                        secondaryButton: .cancel(Text("取消"))
-                                    )
-                                }
-                            
-                            Spacer()
-                        }
+                } else {
+                    VStack {
+                        Spacer()
+                        Image(systemName: "tray.and.arrow.down")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                print("tray.and.arrow.down")
+                                showingScrapAlert = true
+                            }
+                            .alert(isPresented: $showingScrapAlert) {
+                                Alert(
+                                    title: Text("報廢這筆財產？"),
+                                    message: Text("可再還原"),
+                                    primaryButton: .destructive(Text("報廢")) {
+                                        print("Archivebox...")
+                                    },
+                                    secondaryButton: .cancel(Text("取消"))
+                                )
+                            }
+                        
+                        Spacer()
+                        Image(systemName: "trash")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.red)
+                            .onTapGesture {
+                                print("trash")
+                                showingTrashAlert = true
+                            }
+                            .alert(isPresented: $showingTrashAlert) {
+                                Alert(
+                                    title: Text("確定要刪除這筆財產？"),
+                                    message: Text("無法還原"),
+                                    primaryButton: .destructive(Text("刪除")) {
+                                        print("Deleting...")
+                                    },
+                                    secondaryButton: .cancel(Text("取消"))
+                                )
+                            }
+                        
+                        Spacer()
                     }
                 }
-            }
+            }            
         }
     }
 }
